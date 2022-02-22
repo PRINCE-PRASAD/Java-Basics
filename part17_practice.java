1f646b6c3084e57f6fc091c878e119f731e3bfe6
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-02-23 00:28:31 +0530

    practice

---
+++ part17_practice.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class part17_practice {
+    public static void main(String[] args) {
+        //question 1
+        int a = 11;
+        if(a=11){
+
+        }
+
+        // Question 2
+        byte m1, m2, m3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your marks in physis");
+m1 = sc.nextByte();
+
+System.out.println("enter your marks in chemistry");
+m2 = sc.nextByte();
+
+System.out.println("enter your marks in math");
+m3 = sc.nextByte();
+
+float avg = (m1+m2+m3)/3.0f;
+System.out.println("ypur overall persentage is ");
+
+
+
+    }
+    
+}
