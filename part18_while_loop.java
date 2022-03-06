03eca26beb356d08dd5e8bc8d9041946ca52762d
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-03-06 18:24:54 +0530

    new one

---
+++ part18_while_loop.java
@@ -0,0 +1,21 @@
+public class part18_while_loop {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+
+        System.out.println("using loops");
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        } 
+        System.out.println("finish running while loop!");
+
+        
+        // while (true) {
+        //     System.out.println("i am the infinity loop");
+        // }
+    }
+
+}
---
+++ part20_for_loop.java
@@ -0,0 +1,5 @@
+public class part20_for_loop {
+    public static void main(String[] args) {
+         
+    }
+}
