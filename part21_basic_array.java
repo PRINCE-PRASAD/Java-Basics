7cc33c20772328e9f90ce760c31ed461f826e1ff
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-08-09 20:16:04 +0530

    Basic Array initilation

---
+++ part21_basic_array.java
@@ -0,0 +1,28 @@
+public class part21_basic_array {
+    public static void main(String[] args) {
+        // Ways to create array
+
+        // ------------------------------------------------------------
+        // 1. decleration and memory alocation
+        // int [] marks = new int[5];
+        // -----------------------------------------------------------
+
+        // 2. decleration and then memory allocation
+        /*int [] marks;
+        marks = new int[6];
+
+        // insilisation
+        marks[0] = 45;
+        marks[1] = 78;
+        marks[2] = 15;
+        marks[3] = 58;
+        marks[4] = 95;
+        marks[5] = 18;
+         System.out.println(marks[3]);*/
+// ------------------------------------------------------------
+
+        // 3. decleration and then memory allocation and inisilation  together
+        int[] marks = {45, 46, 89, 56, 49};
+         System.out.println(marks[3]);
+    }
+}
