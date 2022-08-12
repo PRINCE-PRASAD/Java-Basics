42954201768f32b417ef5d0cb528fa372c62da17
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-08-12 11:55:37 +0530

    Calling a method or Use of object

---
+++ part26_calling_method_or_function_object.java
@@ -0,0 +1,23 @@
+public class part26_calling_method_or_function_object {
+
+    int logic(int x, int y) {
+        int z;
+        if (x < y) {
+            z = x + y;
+        } else {
+            z = (x - y) * 6;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+
+        part26_calling_method_or_function_object obj = new part26_calling_method_or_function_object();
+        int u = 10;
+        int h = 15;
+        int e;
+        e = obj.logic(u, h);
+        System.out.println(e);
+    }
+}
+// Conclusion-: Here logic is the function inside the class "part26_calling_method_or_function_object" and we need this function inside the  class "public static void main(String[] args)" so we call this class as object in our main class then write |obj."function that we need"|in our main class.
