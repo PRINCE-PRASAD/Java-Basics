8cdb888b39b3eb631f28b1b5037b4d7cbac57c24
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-08-12 14:31:13 +0530

    Calling a method or Use object of function

--- part26_calling_method_or_function_object.java
+++ part26_calling_method_or_function_object.java
@@ -21,3 +21,4 @@ public class part26_calling_method_or_function_object {
     }
 }
 // Conclusion-: Here logic is the function inside the class "part26_calling_method_or_function_object" and we need this function inside the  class "public static void main(String[] args)" so we call this class as object in our main class then write |obj."function that we need"|in our main class.
+// if u don't use static u have to create object for using that function or method.
