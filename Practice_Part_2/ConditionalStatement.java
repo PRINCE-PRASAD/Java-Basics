68eeff0154c5a8ad398e8f0b29f9da7d7af3be4e
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-04-09 20:24:05 +0530

    Conditional Statements

--- Practice_Part_2/ConditionalStatement.java
+++ Practice_Part_2/ConditionalStatement.java
@@ -6,15 +6,16 @@ public class ConditionalStatement {
     public static void main(String[] args) {
         int x = 8;
         int y = 7;
-
+        int z = 9;
+        System.out.println("---------------use of if--------------------");
         if(x>5 && x<10) //if both condition are true 
         System.out.println("everything is ok");
         else
         System.out.println("bye");
 
 
-        System.out.println("-----------------------------------");
-       
+        System.out.println("---------------use of else--------------------");
+         
        
         if(x>y)
         {
@@ -23,5 +24,17 @@ public class ConditionalStatement {
         }
         else
         System.out.println(y);
+
+
+        System.out.println("------------use of else if-----------------------");
+
+        if (x>y && x>z)
+        System.out.println(x);
+
+        else if(y>z)
+        System.out.println(y);
+
+        else 
+        System.out.println(z);
     }
 }
