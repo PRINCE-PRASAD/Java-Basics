e58218f0991abe6819489f6e8777d52bc50e8806
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-04-09 15:27:33 +0530

    operators

---
+++ Practice_Part_2/Assignment_Operaters.java
@@ -0,0 +1,26 @@
+/**
+ * AssignmentOperaters
+ */
+public class Assignment_Operaters {
+
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 5;
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1 % num2);
+        //it will give remain of division
+        System.out.println(num1/num2);
+        // it give Quosent of division
+        System.out.println(num1+5);
+        System.out.println(num1+=5);
+        num1--;
+        System.out.println(num1);
+        num1++;   // post increment 
+        ++num1;   // pre increment
+        System.out.println(num1);
+        // System.out.println(num1--);
+    }
+}
---
+++ Practice_Part_2/RelationalOperator.java
@@ -0,0 +1,25 @@
+
+/**
+ * RelationalOperator
+ */
+public class RelationalOperator {
+public static void main(String[] args) {
+    int x = 9;
+    int y = 4;
+    boolean result = x>y;
+    System.out.println(result);
+    boolean result1 = x<y;
+    // System.out.printf("The answer is %s",result1);
+    System.out.println(result1);
+    boolean result2 = x==y;
+    System.out.println(result2);
+    boolean result3 = x!=y;
+    System.out.println(result3);
+    boolean result4 = x<=y;
+    System.out.println(result4);
+    boolean result5 = x!=y;
+    System.out.println(result5);
+
+}
+    
+}
