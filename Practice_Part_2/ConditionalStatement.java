c8782640c3d3852f27c85ae5a506126f1c81088e
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-04-09 16:20:42 +0530

    Conditional Statement

---
+++ Practice_Part_2/ConditionalStatement.java
@@ -0,0 +1,27 @@
+/**
+ * ConditionalStatement
+ */
+public class ConditionalStatement {
+
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 7;
+
+        if(x>5 && x<10)
+        System.out.println("everything is ok");
+        else
+        System.out.println("bye");
+        
+
+        System.out.println("-----------------------------------");
+       
+       
+        if(x>y)
+        {
+            System.out.println(x);
+            System.err.println("thanks for help");
+        }
+        else
+        System.out.println(y);
+    }
+}
