16d3c568164458474f7c363936c48d2c6f5713d9
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-04-14 22:19:03 +0530

    Constructor with and witout parameters

--- Practice_Part_2/Constructer.java
+++ Practice_Part_2/Constructer.java
@@ -1,11 +1,16 @@
-class Human{
+// class Human extends Object   // orignal hidden data
+class Human
+{
     public String name;
     public int age;
 
-    public Human(){     // 1. Constructor dont have any return type.
+    public Human()
+    {   //super();      // orignal hidden data of constructor
+                        // 1. Constructor dont have any return type.
         name = "Rina";  // 2. Constructor has same name of class that it belong from
         age = 56;       // 3. whenever you create the obj by default you get the constructor data 
     }                   // 4. By default we always have a blank constructor 
+                        // 5 whenever we create the constructor there is a default super class "super()" i it.
 
     public Human(int a, String n){  // constructor with parameters
        name = n;
